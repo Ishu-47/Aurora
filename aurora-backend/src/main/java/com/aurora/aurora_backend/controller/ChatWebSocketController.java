58c89c59c3f71ec3bec693cb66f8ac3f4c03f629
Ishu-47 <kumarrajoraishu@gmail.com>
2026-06-11 17:54:11 +0530

    feat: add ChatWebSocketController for real-time messaging; implement sendMessage functionality to handle message broadcasting

---
+++ aurora-backend/src/main/java/com/aurora/aurora_backend/controller/ChatWebSocketController.java
@@ -0,0 +1,51 @@
+package com.aurora.aurora_backend.controller;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import com.aurora.aurora_backend.dto.MessageResponseDTO;
+import com.aurora.aurora_backend.dto.SendMessageRequestDTO;
+import com.aurora.aurora_backend.entity.Conversation;
+import com.aurora.aurora_backend.entity.ConversationParticipant;
+import com.aurora.aurora_backend.entity.User;
+import com.aurora.aurora_backend.repository.ConversationParticipantRepository;
+import com.aurora.aurora_backend.repository.ConversationRepository;
+import com.aurora.aurora_backend.repository.UserRepository;
+import com.aurora.aurora_backend.service.ConversationService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class ChatWebSocketController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+    private final UserRepository userRepository;
+    private final ConversationRepository conversationRepository;
+    private final ConversationParticipantRepository participantRepository;
+    private final ConversationService conversationService;
+
+    @MessageMapping("/chat.send")
+    public void sendMessage(SendMessageRequestDTO request, Principal principal) {
+        User sender = userRepository.findByEmail(principal.getName()).orElseThrow();
+
+        MessageResponseDTO message = conversationService.createMessage(sender, request.conversationId(),
+                request.content());
+        Conversation conversation = conversationRepository.findById(request.conversationId()).orElseThrow();
+
+        List<ConversationParticipant> participants = participantRepository.findByConversation(conversation);
+        for (ConversationParticipant participant : participants) {
+            messagingTemplate.convertAndSendToUser(
+                    participant
+                            .getUser()
+                            .getEmail(),
+                    "/queue/messages",
+                    message);
+        }
+        
+    }
+}
--- aurora-backend/src/main/java/com/aurora/aurora_backend/service/ConversationService.java
+++ aurora-backend/src/main/java/com/aurora/aurora_backend/service/ConversationService.java
@@ -24,159 +24,167 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ConversationService {
 
-    private final UserRepository userRepository;
-    private final ConversationRepository conversationRepository;
-    private final ConversationParticipantRepository participantRepository;
-    private final MessageRepository messageRepository;
+        private final UserRepository userRepository;
+        private final ConversationRepository conversationRepository;
+        private final ConversationParticipantRepository participantRepository;
+        private final MessageRepository messageRepository;
 
-    public ConversationResponseDTO createOrGetConversation(String targetUsername) {
-        String currentEmail = SecurityContextHolder.getContext().getAuthentication().getName();
-        User currentUser = userRepository.findByEmail(currentEmail)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-        User targetUser = userRepository.findByUsername(targetUsername)
-                .orElseThrow(() -> new RuntimeException("Target user not found"));
+        public ConversationResponseDTO createOrGetConversation(String targetUsername) {
+                String currentEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+                User currentUser = userRepository.findByEmail(currentEmail)
+                                .orElseThrow(() -> new RuntimeException("User not found"));
+                User targetUser = userRepository.findByUsername(targetUsername)
+                                .orElseThrow(() -> new RuntimeException("Target user not found"));
 
-        if (currentUser.getId().equals(targetUser.getId())) {
-            throw new RuntimeException("Cannot create conversation with yourself");
-        }
-        String conversationKey = buildConversationKey(currentUser.getId(), targetUser.getId());
-        Conversation conversation = conversationRepository.findByConversationKey(conversationKey)
-                .orElseGet(() -> createConversation(conversationKey, currentUser, targetUser));
-        return new ConversationResponseDTO(conversation.getId());
+                if (currentUser.getId().equals(targetUser.getId())) {
+                        throw new RuntimeException("Cannot create conversation with yourself");
+                }
+                String conversationKey = buildConversationKey(currentUser.getId(), targetUser.getId());
+                Conversation conversation = conversationRepository.findByConversationKey(conversationKey)
+                                .orElseGet(() -> createConversation(conversationKey, currentUser, targetUser));
+                return new ConversationResponseDTO(conversation.getId());
 
-    }
-
-    private String buildConversationKey(Long user1Id, Long user2Id) {
-        long first = Math.min(user1Id, user2Id);
-        long second = Math.max(user1Id, user2Id);
-        return first + "_" + second;
-    }
-
-    private Conversation createConversation(String conversationKey, User user1, User user2) {
-        Conversation conversation = new Conversation();
-        conversation.setConversationKey(conversationKey);
-        conversation = conversationRepository.save(conversation);
-
-        ConversationParticipant p1 = new ConversationParticipant();
-        p1.setConversation(conversation);
-        p1.setUser(user1);
-        ConversationParticipant p2 = new ConversationParticipant();
-        p2.setConversation(conversation);
-        p2.setUser(user2);
-
-        participantRepository.save(p1);
-        participantRepository.save(p2);
-
-        return conversation;
-    }
-
-    public List<ConversationListItemDTO> getConversations() {
-        String currentEmail = SecurityContextHolder.getContext().getAuthentication().getName();
-        User currentUser = userRepository.findByEmail(currentEmail)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-        List<ConversationParticipant> participations = participantRepository.findByUser(currentUser);
-
-        return participations.stream().map(participation -> {
-            Conversation conversation = participation.getConversation();
-            User otherUser = participantRepository.findByConversation(conversation).stream()
-                    .map(ConversationParticipant::getUser).filter(user -> !user.getId().equals(currentUser.getId()))
-                    .findFirst().orElse(null);
-            Message lastMessage = messageRepository.findTopByConversationOrderByCreatedAtDesc(conversation)
-                    .orElse(null);
-
-            return new ConversationListItemDTO(conversation.getId(), otherUser != null
-                    ? otherUser.getDisplayUsername()
-                    : "Unknown User",
-                    otherUser != null ? otherUser.getProfilePictureUrl() : null,
-                    lastMessage != null ? lastMessage.getContent() : null,
-                    lastMessage != null ? lastMessage.getCreatedAt() : null);
-        }).sorted(Comparator.comparing(ConversationListItemDTO::lastMessageTime,
-                Comparator.nullsLast(Comparator.reverseOrder()))).toList();
-    }
-
-    public List<MessageResponseDTO> getMessages(Long conversationId) {
-
-        String currentEmail = SecurityContextHolder.getContext()
-                .getAuthentication()
-                .getName();
-
-        User currentUser = userRepository
-                .findByEmail(currentEmail)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-
-        Conversation conversation = conversationRepository
-                .findById(conversationId)
-                .orElseThrow(() -> new RuntimeException("Conversation not found"));
-
-        boolean participant = participantRepository
-                .existsByConversationAndUser(conversation, currentUser);
-
-        if (!participant) {
-            throw new RuntimeException(
-                    "Access denied");
         }
 
-        return messageRepository
-                .findByConversationOrderByCreatedAtAsc(conversation)
-                .stream()
-                .map(message -> new MessageResponseDTO(
-                        message.getId(),
-                        message.getSender().getId(),
-                        message.getSender().getDisplayUsername(),
-                        message.getContent(),
-                        message.getCreatedAt()))
-                .toList();
-    }
-
-    public MessageResponseDTO sendMessage(
-            SendMessageRequestDTO request) {
-
-        String currentEmail = SecurityContextHolder
-                .getContext()
-                .getAuthentication()
-                .getName();
-
-        User sender = userRepository
-                .findByEmail(currentEmail)
-                .orElseThrow(() -> new RuntimeException(
-                        "User not found"));
-
-        Conversation conversation = conversationRepository
-                .findById(
-                        request.conversationId())
-                .orElseThrow(() -> new RuntimeException(
-                        "Conversation not found"));
-
-        boolean participant = participantRepository
-                .existsByConversationAndUser(
-                        conversation,
-                        sender);
-
-        if (!participant) {
-            throw new RuntimeException(
-                    "Access denied");
+        private String buildConversationKey(Long user1Id, Long user2Id) {
+                long first = Math.min(user1Id, user2Id);
+                long second = Math.max(user1Id, user2Id);
+                return first + "_" + second;
         }
 
-        Message message = new Message();
+        private Conversation createConversation(String conversationKey, User user1, User user2) {
+                Conversation conversation = new Conversation();
+                conversation.setConversationKey(conversationKey);
+                conversation = conversationRepository.save(conversation);
 
-        message.setConversation(
-                conversation);
+                ConversationParticipant p1 = new ConversationParticipant();
+                p1.setConversation(conversation);
+                p1.setUser(user1);
+                ConversationParticipant p2 = new ConversationParticipant();
+                p2.setConversation(conversation);
+                p2.setUser(user2);
 
-        message.setSender(
-                sender);
+                participantRepository.save(p1);
+                participantRepository.save(p2);
 
-        message.setContent(
-                request.content().trim());
+                return conversation;
+        }
 
-        message = messageRepository.save(
-                message);
+        public List<ConversationListItemDTO> getConversations() {
+                String currentEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+                User currentUser = userRepository.findByEmail(currentEmail)
+                                .orElseThrow(() -> new RuntimeException("User not found"));
+                List<ConversationParticipant> participations = participantRepository.findByUser(currentUser);
 
-        return new MessageResponseDTO(
-                message.getId(),
-                sender.getId(),
-                sender.getDisplayUsername(),
-                message.getContent(),
-                message.getCreatedAt());
-    }
+                return participations.stream().map(participation -> {
+                        Conversation conversation = participation.getConversation();
+                        User otherUser = participantRepository.findByConversation(conversation).stream()
+                                        .map(ConversationParticipant::getUser)
+                                        .filter(user -> !user.getId().equals(currentUser.getId()))
+                                        .findFirst().orElse(null);
+                        Message lastMessage = messageRepository.findTopByConversationOrderByCreatedAtDesc(conversation)
+                                        .orElse(null);
+
+                        return new ConversationListItemDTO(conversation.getId(), otherUser != null
+                                        ? otherUser.getDisplayUsername()
+                                        : "Unknown User",
+                                        otherUser != null ? otherUser.getProfilePictureUrl() : null,
+                                        lastMessage != null ? lastMessage.getContent() : null,
+                                        lastMessage != null ? lastMessage.getCreatedAt() : null);
+                }).sorted(Comparator.comparing(ConversationListItemDTO::lastMessageTime,
+                                Comparator.nullsLast(Comparator.reverseOrder()))).toList();
+        }
+
+        public List<MessageResponseDTO> getMessages(Long conversationId) {
+
+                String currentEmail = SecurityContextHolder.getContext()
+                                .getAuthentication()
+                                .getName();
+
+                User currentUser = userRepository
+                                .findByEmail(currentEmail)
+                                .orElseThrow(() -> new RuntimeException("User not found"));
+
+                Conversation conversation = conversationRepository
+                                .findById(conversationId)
+                                .orElseThrow(() -> new RuntimeException("Conversation not found"));
+
+                boolean participant = participantRepository
+                                .existsByConversationAndUser(conversation, currentUser);
+
+                if (!participant) {
+                        throw new RuntimeException(
+                                        "Access denied");
+                }
+
+                return messageRepository
+                                .findByConversationOrderByCreatedAtAsc(conversation)
+                                .stream()
+                                .map(message -> new MessageResponseDTO(
+                                                message.getId(),
+                                                message.getSender().getId(),
+                                                message.getSender().getDisplayUsername(),
+                                                message.getContent(),
+                                                message.getCreatedAt()))
+                                .toList();
+        }
+
+        public MessageResponseDTO sendMessage(
+                        SendMessageRequestDTO request) {
+
+                String currentEmail = SecurityContextHolder
+                                .getContext()
+                                .getAuthentication()
+                                .getName();
+
+                User sender = userRepository
+                                .findByEmail(currentEmail)
+                                .orElseThrow(() -> new RuntimeException(
+                                                "User not found"));
+
+                return createMessage(sender, request.conversationId(), request.content());
+        }
+
+        public MessageResponseDTO createMessage(
+                        User sender,
+                        Long conversationId,
+                        String content) {
+
+                Conversation conversation = conversationRepository
+                                .findById(conversationId)
+                                .orElseThrow(() -> new RuntimeException(
+                                                "Conversation not found"));
+
+                boolean participant = participantRepository
+                                .existsByConversationAndUser(
+                                                conversation,
+                                                sender);
+
+                if (!participant) {
+                        throw new RuntimeException(
+                                        "Access denied");
+                }
+
+                Message message = new Message();
+
+                message.setConversation(
+                                conversation);
+
+                message.setSender(
+                                sender);
+
+                message.setContent(
+                                content.trim());
+
+                message = messageRepository.save(
+                                message);
+
+                return new MessageResponseDTO(
+                                message.getId(),
+                                sender.getId(),
+                                sender.getDisplayUsername(),
+                                message.getContent(),
+                                message.getCreatedAt());
+        }
 
 }
